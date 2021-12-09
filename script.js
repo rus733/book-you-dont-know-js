1926c735d709e0632699ce07a76a4f39e31c8b98
rus733 <9282716617@mail.ru>
2021-12-09 19:41:39 +0300

    расставил комменты  дз10

--- script.js
+++ script.js
@@ -3,18 +3,25 @@
 const books = document.querySelectorAll(".book");
 const body = document.querySelector("body");
 
-//расставили книги по порядку
+// 1.Восстановить порядок книг.
 books[0].before(books[1]);
 books[3].before(books[4]);
 books[3].before(books[2]);
 books[2].before(books[3]);
 books[2].before(books[5]);
 
-// меняем картику заднего фона
+// 2. Заменить картинку заднего фона на другую из папки image
+
 //console.dir(body.style);
 body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
 
-// убираем рекламу
+// 3.Исправить заголовок в книге 3
+
+const links = document.getElementsByTagName("a");
+//console.log(links[2]);
+links[2].textContent = "Книга 3. this и Прототипы Объектов";
+
+// 4. убираем рекламу
 
 const adv = document.querySelector(".adv");
 //console.log(adv);
@@ -22,13 +29,7 @@ adv.classList.toggle("adv");
 //adv.classList.remove("adv");
 //console.log(adv);
 
-// правим заголовок
-
-const links = document.getElementsByTagName("a");
-//console.log(links[2]);
-links[2].textContent = "Книга 3. this и Прототипы Объектов";
-
-//Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
+// 5. Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
 
 const lists = document.querySelectorAll("ul");
 console.dir(lists);
@@ -46,7 +47,7 @@ lists[4].children[1].after(lists[4].children[9]);
 lists[4].children[5].after(lists[4].children[3]);
 lists[4].children[8].after(lists[4].children[6]);
 
-// в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+// 6. в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
 
 const newElem = document.createElement("li"); //- создаем элемент указывая его тег li
 newElem.textContent = "Глава 8: За пределами ES6"; // дадим ему текст содержание

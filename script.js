7f7e091ebe09e89c06dcf5cf96fe4c0d16cbd118
rus733 <9282716617@mail.ru>
2021-12-09 20:05:17 +0300

    расставил названия заданий  дз10

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// занесем  в  переменную элементы с классом .book
+// Домашнее задание 10.Тема Работа с элементами DOM.
 
 const books = document.querySelectorAll(".book");
 const body = document.querySelector("body");
+const links = document.getElementsByTagName("a");
 
 // 1.Восстановить порядок книг.
+
 books[0].before(books[1]);
 books[3].before(books[4]);
 books[3].before(books[2]);
@@ -11,33 +13,23 @@ books[2].before(books[3]);
 books[2].before(books[5]);
 
 // 2. Заменить картинку заднего фона на другую из папки image
-
-//console.dir(body.style);
 body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
 
 // 3.Исправить заголовок в книге 3
 
-const links = document.getElementsByTagName("a");
-//console.log(links[2]);
 links[2].textContent = "Книга 3. this и Прототипы Объектов";
 
 // 4. убираем рекламу
 
 const adv = document.querySelector(".adv");
-//console.log(adv);
 adv.classList.toggle("adv");
+//второй вариант
 //adv.classList.remove("adv");
-//console.log(adv);
 
 // 5. Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
 
 const lists = document.querySelectorAll("ul");
-console.dir(lists);
-console.dir(lists[1]);
-console.dir(lists[1].children[0]);
-
 const listsItem = document.querySelectorAll("li");
-console.dir(listsItem);
 
 lists[1].children[4].before(lists[1].children[6]);
 lists[1].children[5].before(lists[1].children[8]);
@@ -49,10 +41,7 @@ lists[4].children[8].after(lists[4].children[6]);
 
 // 6. в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
 
-const newElem = document.createElement("li"); //- создаем элемент указывая его тег li
-newElem.textContent = "Глава 8: За пределами ES6"; // дадим ему текст содержание
-
-lists[5].append(newElem); //добавим его в второй список
-
-console.log(newElem);
+const newElem = document.createElement("li");
+newElem.textContent = "Глава 8: За пределами ES6";
+lists[5].append(newElem); //добавим его в список
 lists[5].children[8].after(lists[5].children[10]);
--- script0.js
+++ script0.js
@@ -64,7 +64,9 @@ const title = document.getElementById("title"); // получим элемент
 
 /// метод добавления текста в теги
 
-///title.insertAdjacentText("beforebegin", "beforebegin"); // вставим текст по условию - beforebegin текст для примера такой же beforebegin
+/*
+title.insertAdjacentText("beforebegin", "beforebegin"); // вставим текст по условию - "beforebegin" текст для примера такой же 
+*/
 
 //title.insertAdjacentText("afterend", "afterend");
 //title.insertAdjacentText("beforeend", " beforeend ");
@@ -96,3 +98,63 @@ lists[1].insertBefore(elems[5], elems[1]); //before
 lists[1].replaceChild(elems[5], elems[1]); //replaceWith
 
 lists[1].removeChild(elems[5]); //remove
+
+// Домашнее задание 10.Тема Работа с элементами DOM.
+
+const books = document.querySelectorAll(".book");
+const body = document.querySelector("body");
+
+// 1.Восстановить порядок книг.
+
+books[0].before(books[1]);
+books[3].before(books[4]);
+books[3].before(books[2]);
+books[2].before(books[3]);
+books[2].before(books[5]);
+
+// 2. Заменить картинку заднего фона на другую из папки image
+
+//console.dir(body.style);
+body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
+
+// 3.Исправить заголовок в книге 3
+
+const links = document.getElementsByTagName("a");
+//console.log(links[2]);
+links[2].textContent = "Книга 3. this и Прототипы Объектов";
+
+// 4. убираем рекламу
+
+const adv = document.querySelector(".adv");
+//console.log(adv);
+adv.classList.toggle("adv");
+//adv.classList.remove("adv");
+//console.log(adv);
+
+// 5. Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
+
+//const lists = document.querySelectorAll("ul");
+console.dir(lists);
+console.dir(lists[1]);
+console.dir(lists[1].children[0]);
+
+const listsItem = document.querySelectorAll("li");
+console.dir(listsItem);
+
+lists[1].children[4].before(lists[1].children[6]);
+lists[1].children[5].before(lists[1].children[8]);
+lists[1].children[10].before(lists[1].children[2]);
+
+lists[4].children[1].after(lists[4].children[9]);
+lists[4].children[5].after(lists[4].children[3]);
+lists[4].children[8].after(lists[4].children[6]);
+
+// 6. в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+
+const newElem = document.createElement("li"); //- создаем элемент указывая его тег li
+newElem.textContent = "Глава 8: За пределами ES6"; // дадим ему текст содержание
+
+lists[5].append(newElem); //добавим его в второй список
+
+console.log(newElem);
+lists[5].children[8].after(lists[5].children[10]);
